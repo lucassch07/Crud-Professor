7d990c65127b3050a7f00fa9bcfbb1d7d1391fa4
lucassch07 <lucasschowambach0311@gmail.com>
2025-05-26 16:36:59 -0300

    Att

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -1,33 +1,33 @@
 <?php
     include "../config/config.inc.php";
 
-class Database{
+    class Database{
 
-    public static function abrirConexao(){
-        try{
-            return new PDO(DSN, USUARIO, SENHA);
-        }catch(PDOException $e){
-            echo "Erro ao conectar com o banco de dados. ".$e->getMessage();
+        public static function abrirConexao(){
+            try{
+                return new PDO(DSN, USUARIO, SENHA);
+            }catch(PDOException $e){
+                echo "Erro ao conectar com o banco de dados. ".$e->getMessage();
+            }
+        }
+
+        public static function preparar($sql){
+            $conexao = self::abrirConexao();
+            return $conexao->prepare($sql);
+        }
+
+        public static function vincularParametros($comando, $parametros){
+            foreach($parametros as $chave=>$valor){
+                $comando->bindValue($chave, $valor);
+            }
+            return $comando;
+        }
+
+        public static function executar($sql, $parametros){
+            $comando = self::preparar($sql);
+            $comando = self::vincularParametros($comando, $parametros);
+            $comando->execute();
+            return $comando;
         }
     }
-
-    public static function preparar($sql){
-        $conexao = self::abrirConexao();
-        return $conexao->prepare($sql);
-    }
-
-    public static function vincularParametros($comando, $parametros){
-        foreach($parametros as $chave=>$valor){
-            $comando->bindValue($chave, $valor);
-        }
-        return $comando;
-    }
-
-    public static function executar($sql, $parametros){
-        $comando = self::preparar($sql);
-        $comando = self::vincularParametros($comando, $parametros);
-        $comando->execute();
-        return $comando;
-    }
-}
 ?>
